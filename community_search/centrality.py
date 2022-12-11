9760e31a92b6946f0075249be1ea2dc1a02523c5
Michael Assmair <assmair.michael@gmail.com>
2022-12-11 16:38:39 +0100

    Cleanup centrality.py

--- community_search/centrality.py
+++ community_search/centrality.py
@@ -2,10 +2,7 @@
 
 import networkx as nx
 from collections import deque
-import random
-import heapq
 from queue import PriorityQueue
-import timeit
 
 def girvan_newman(G: nx.Graph):
     """Algorithmus berechnet Communitys des Graphen G"""
@@ -27,8 +24,6 @@ def _edge_betweenness(G: nx.Graph) -> dict:
     """berechnet die edge betweenness"""
 
     edges = {edge: 0.0 for edge in G.edges}
-
-    start_time = timeit.default_timer()
 
     for node in G.nodes:
         path_graph = nx.DiGraph()
@@ -53,9 +48,7 @@ def _edge_betweenness(G: nx.Graph) -> dict:
                 elif path_graph.nodes[adj_node]["dist"] == path_graph.nodes[next_node]["dist"] + 1:
                     path_graph.nodes[adj_node]["weight"] += path_graph.nodes[next_node]["weight"]
                     path_graph.add_edge(next_node, adj_node)
-                
-        print("path", timeit.default_timer() - start_time)
-        start_time = timeit.default_timer()
+
 
         node_heap = PriorityQueue()
         for node in (node for node in path_graph.nodes if path_graph.out_degree(node) == 0):
@@ -87,7 +80,5 @@ def _edge_betweenness(G: nx.Graph) -> dict:
                 edges[edge] += path_graph.edges[edge]["betweenness"]/2
             else:
                 edges[(edge[1], edge[0])] += path_graph.edges[edge]["betweenness"]/2
-
-        print("between", timeit.default_timer() - start_time)
         
     return edges
