a26eb14da6f87016cee16793bf0ccb6b3d52a486
Michael Assmair <assmair.michael@gmail.com>
2022-12-11 22:53:27 +0100

    Update label_propagation

--- community_search/label_propagation.py
+++ community_search/label_propagation.py
@@ -1,29 +1,55 @@
-"""label propagation is an algorithm to find communities in large networks"""
+"""Label propagation."""
+
+# Author: Michael Assmair
 
 import random
 import networkx as nx
 from networkx.utils import groups
 
-def lpa_communities(G: nx.Graph, weight=None, seed=None):
-    """Uses the asynchrones label propagation algorithm to find communities
-    in the graph G
 
-    Return: communities: iteratable
+def lpa_communities(G, weight=None, seed=None):
+    """Sucht nach Communitys im Graphen G.
+
+    Implementierung eines asynchronen Label propagation Algorithmus,
+    wie in https://arxiv.org/abs/0709.2938 beschrieben.
+
+    Parameters
+    ----------
+    G : Graph im Networkx-Format. Der Algorithmus funktioniert sowol
+        mit gerichteten als auch mit ungerichteten Graphen.
+
+    weight : Kantengewichte, die bei der berechnung der Communitys
+        verwendet werden. Sollte kein Kantengewicht angegeben werden,
+        so erhält jede Kante das Gewicht w=1.
+
+    seed : Seed, der für den Pseudozufallszahlengenerator verwendet wird.
+        Sollte kein Seed übergene werden, wird die Standardeinstellung des
+        Paketes 'random' verwendet.
+
+    Returns
+    -------
+    communities : Dictionary mit einem Label als Schlüsses und einem
+        Set von Knoten aus der selben Community als Wert. Wenn G kein
+        Networkx Graph ist, wird ein leeres Set zurückgegeben.
     """
-
     if not (type(G) == nx.DiGraph or type(G) == nx.Graph):
         return set()
+
+    random.seed(seed)
     
+    #Die Startlabel werden vergeben.
     labels = {node: label for (label, node) in enumerate(G)}
     nodes = list(labels)
-    random.seed(seed)
 
     finished = False
-
+    
+    #Die Schleife läuft, bis alle Knoten das selbe Label haben
+    #wie die meisten seiner Nachbarknoten.
     while not finished:
         finished = True
         random.shuffle(nodes)
 
+        #Knoten werden asynchron durchlaufen.
         for node in nodes:
             neighbor_labels = {label: 0 for label in labels.values()}
 
@@ -35,11 +61,17 @@ def lpa_communities(G: nx.Graph, weight=None, seed=None):
             
             max_count = max(neighbor_labels.values())
             max_neighbor_labels = [k for k,v in neighbor_labels.items() if v == max_count]
-
+            
+            #Wenn der aktuell bearbeitete Knoten nicht ein Label hat
+            #wie die meisten seiner Nachbarschaft, wird dich Schleife
+            #moch mindestens einmal durchlaufen.
             if labels[node] not in max_neighbor_labels:
                 finished = False
             
             labels[node] = random.choice(max_neighbor_labels)
 
+    #Die Methode 'groups' bildet alle Knoten mit dem selben
+    #Label auf ein Set ab.
+    communities = groups(labels).values()
 
-    yield from groups(labels).values()
+    return communities
